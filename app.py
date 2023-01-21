ec513c90765603557c34ce96d10395380f904f30
Ansh Nanda <anshnanda10@gmail.com>
2023-01-21 14:47:44 -0800

    basic /prompt endpoint

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+# SETUP
+from flask import Flask, request, jsonify
+import ask_fsdl
+from dotenv import load_dotenv
+load_dotenv()
+run_query = ask_fsdl.get_runner()
+
+
+# CREATE FLASK APP AND ROUTES
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+
+@app.route("/prompt", methods=['POST'])
+def prompt():
+    content = request.get_json()
+    text = content['text']
+    response = run_query(text)  # execute
+    return jsonify(response)
