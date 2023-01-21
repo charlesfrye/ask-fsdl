4dcf7343d0835dc17c53f9efb78a6ea9d1634979
Charles Frye <cfrye59@gmail.com>
2023-01-21 22:09:41 +0000

    adds lecture downloading code, thanks chatGPT

--- ask_fsdl.py
+++ ask_fsdl.py
@@ -24,56 +24,74 @@ def make_llm(model_name="text-davinci-003", temperature=0.):
   return llm
 
 
+def download_lectures(lecture_md_urls):
+    import os
+    import subprocess
+
+    for idx, url in lecture_md_urls.items():
+        filename = "documents/lecture-{}.md".format(str(idx).zfill(2))
+        if not os.path.exists(filename):
+          subprocess.run(["wget","-O", filename, url], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
 def produce_documents():
-  import os
-  from pathlib import Path
-  import string
+    import os
+    from pathlib import Path
+    import shutil
+    import string
 
-  lecture_titles = {
-    1: "lecture-1-course-vision-and-when-to-use-ml",
-    2: "lecture-2-development-infrastructure-and-tooling",
-    3: "lecture-3-troubleshooting-and-testing",
-    4: "lecture-4-data-management",
-    5: "lecture-5-deployment",
-    6: "lecture-6-continual-learning",
-    7: "lecture-7-foundation-models",
-    8: "lecture-8-teams-and-pm",
-    9: "lecture-9-ethics"
-  }
+    if os.path.exists("documents"):
+        shutil.rmtree("documents")
 
-  lecture_md_url_base = "https://raw.githubusercontent.com/full-stack-deep-learning/website/main/docs/course/2022/"
+    if not os.path.exists("documents"):
+        os.mkdir("documents")
 
-  lecture_md_urls = {idx: lecture_md_url_base + title + "/index.md" for idx, title in lecture_titles.items()}
+    lecture_titles = {
+      1: "lecture-1-course-vision-and-when-to-use-ml",
+      2: "lecture-2-development-infrastructure-and-tooling",
+      3: "lecture-3-troubleshooting-and-testing",
+      4: "lecture-4-data-management",
+      5: "lecture-5-deployment",
+      6: "lecture-6-continual-learning",
+      7: "lecture-7-foundation-models",
+      8: "lecture-8-teams-and-pm",
+      9: "lecture-9-ethics"
+    }
 
-  lecture_md_filenames = [elem for elem in os.listdir("documents") if "lecture" in elem]
+    lecture_md_url_base = "https://raw.githubusercontent.com/full-stack-deep-learning/website/main/docs/course/2022/"
 
+    lecture_md_urls = {idx: lecture_md_url_base + title + "/index.md" for idx, title in lecture_titles.items()}
 
-  docs_folder = Path("documents")
-  lecture_texts = {}
+    download_lectures(lecture_md_urls)
 
+    lecture_md_filenames = [elem for elem in os.listdir("documents") if "lecture" in elem]
 
-  for fn in lecture_md_filenames:
-      idx = int("".join(elem for elem in fn if elem in string.digits))
-      lecture_md_path = docs_folder / fn
-      with open(lecture_md_path) as f:
-          lecture = f.read()
-          lecture_texts[idx] = lecture
-
-  from langchain.text_splitter import CharacterTextSplitter
+    docs_folder = Path("documents")
+    lecture_texts = {}
 
 
-  text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    for fn in lecture_md_filenames:
+        idx = int("".join(elem for elem in fn if elem in string.digits))
+        lecture_md_path = docs_folder / fn
+        with open(lecture_md_path) as f:
+            lecture = f.read()
+            lecture_texts[idx] = lecture
 
-  lecture_texts_split = {idx: text_splitter.split_text(lecture_text) for idx, lecture_text in lecture_texts.items()}
+    from langchain.text_splitter import CharacterTextSplitter
 
-  website_url_base = "https://fullstackdeeplearning.com/course/2022/"
-  source_urls = {idx: website_url_base + title for idx, title in lecture_titles.items()}
-  source_urls_split = {"source": [source_urls[idx]] * len(splits) for idx, splits in lecture_texts_split.items()}
 
-  lecture_texts_flat = [split for lecture_text in lecture_texts_split.values() for split in lecture_text]
-  source_urls_flat = [{"source": source_urls[idx]} for idx, lecture_text in lecture_texts_split.items() for split in lecture_text]
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
-  return lecture_texts_flat, source_urls_flat
+    lecture_texts_split = {idx: text_splitter.split_text(lecture_text) for idx, lecture_text in lecture_texts.items()}
+
+    website_url_base = "https://fullstackdeeplearning.com/course/2022/"
+    source_urls = {idx: website_url_base + title for idx, title in lecture_titles.items()}
+    source_urls_split = {"source": [source_urls[idx]] * len(splits) for idx, splits in lecture_texts_split.items()}
+
+    lecture_texts_flat = [split for lecture_text in lecture_texts_split.values() for split in lecture_text]
+    source_urls_flat = [{"source": source_urls[idx]} for idx, lecture_text in lecture_texts_split.items() for split in lecture_text]
+
+    return lecture_texts_flat, source_urls_flat
 
 
 def embed_texts(texts, metadatas):
