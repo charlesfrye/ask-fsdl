3c8a819516054a654534623d39a7d0909059fe43
Charles Frye <cfrye59@gmail.com>
2023-01-21 19:23:03 -0800

    scopes bot to only a few channels

--- run_bot.py
+++ run_bot.py
@@ -17,12 +17,21 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)  # connection to Discord
 
+TARGETED_CHANNELS = [
+    1066450466898186382, # dev channel: `ask-fsdl-dev`
+    1066557596313604200, # main channel: `ask-fsdl`
+    984528990368825395,  # `instructor-lounge`
+]
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
+    if message.channel.id not in TARGETED_CHANNELS:
+        return
+
     if message.author == client.user:  # ignore posts by self
         return
 
