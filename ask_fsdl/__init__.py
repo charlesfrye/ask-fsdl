0f5e9dc7861d7ab2e7fea7f2cf77b60d3d183ea5
Charles Frye <cfrye59@gmail.com>
2023-01-22 00:38:56 +0000

    persists docstore in .pkl

--- ask_fsdl/__init__.py
+++ ask_fsdl/__init__.py
@@ -2,14 +2,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import os
+
 from . import make_docs
 from .chainrunner import *
 from .docstore import *
 
 
-def get_runner():
-  texts, metadatas = make_docs.produce_documents()
-  docsearch = FaissDocumentStore(texts, metadatas)
+def get_runner(regenerate=False):
+  from pathlib import Path
+  filename = Path(FaissDocumentStore.filename).resolve()
+
+  if regenerate or not os.path.exists(filename):
+      texts, metadatas = make_docs.produce_documents()
+      docsearch = FaissDocumentStore.from_texts(texts, metadatas)
+      docsearch.to_pickle()
+  else:
+      docsearch = FaissDocumentStore.from_pickle()
 
   chain = StuffChain({"model_name": "text-davinci-003", "temperature": 0.0})
 
--- ask_fsdl/docstore.py
+++ ask_fsdl/docstore.py
@@ -1,7 +1,5 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
-
 from langchain.vectorstores.faiss import FAISS
-# https://github.com/facebookresearch/faiss
 
 
 class DocumentStore:
@@ -11,20 +9,38 @@ class DocumentStore:
 
 
 class FaissDocumentStore(DocumentStore):
+  filename = "documents/stored.pkl"
 
-  def __init__(self, documents, metadatas):
-    self.documents = documents
-    self.metadatas = metadatas
+  def __init__(self, store):
+    self.store = store
 
-    self.setup()
+  @classmethod
+  def from_texts(cls, texts, metadatas):
+    embeddings = OpenAIEmbeddings()  # uses ada-002 by default
+    docsearch = FAISS.from_texts(texts, embeddings, metadatas=metadatas)
 
-  def setup(self):
+    return cls(docsearch)
 
-    self.embeddings = OpenAIEmbeddings()  # uses ada-002 by default
+  @classmethod
+  def from_pickle(cls, filename=None):
+    import pickle
 
-    docsearch = FAISS.from_texts(self.documents, self.embeddings, metadatas=self.metadatas)
+    if filename is None:
+      filename = cls.filename
 
-    self._store = docsearch
+    with open(filename, "rb") as f:
+      store = pickle.load(f)
+
+    return cls(store)
+
+  def to_pickle(self, filename=None):
+    import pickle
+
+    if filename is None:
+      filename = self.filename
+
+    with open(filename, "wb") as f:
+      pickle.dump(self.store, f)
 
   def __getitem__(self, query):
-    return self._store.similarity_search(query)
+    return self.store.similarity_search(query)
