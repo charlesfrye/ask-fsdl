12d298d88797000a698193839598a912a5a080b4
Ansh Nanda <anshnanda10@gmail.com>
2023-01-21 17:00:38 -0800

    basic frontend q&a bot setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,30 +3,67 @@ import "./App.css";
 
 function App() {
   const [searchText, setSearchText] = useState("");
+  const [response, setResponse] = useState<string>();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async (e: any) => {
+    setIsLoading(true);
     e.preventDefault();
     console.log(`Searching for ${searchText}`);
-    const res = await fetch("/hello");
+    const res = await fetch("/prompt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: searchText,
+        config: {
+          type: "WEB",
+        },
+      }),
+    });
     const data = await res.json();
+
+    setIsLoading(false);
+    setResponse(data);
     console.log(data);
   };
   return (
     <div className="App">
       <h1>Ask FSDL</h1>
 
-      <form className="fancy-search-bar" onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          className="fancy-search-bar__input"
-          placeholder="What do you want to know?..."
-        />
-        <button className="fancy-search-bar__button">Search</button>
-      </form>
+      {response ? (
+        <h4>{response}</h4>
+      ) : (
+        <form className="fancy-search-bar" onSubmit={handleSearch}>
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="fancy-search-bar__input"
+            placeholder="What do you want to know?..."
+            disabled={isLoading}
+          />
+          <button type="button" disabled={isLoading} style={{ backgroundColor: "#646cff" }}>
+            Search
+          </button>
+        </form>
+      )}
 
+      {isLoading && <h3>Loading...</h3>}
 
+      {response && (
+        <button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            setResponse(undefined);
+            setIsLoading(false);
+          }}
+        >
+          Ask another question
+        </button>
+      )}
     </div>
   );
 }
